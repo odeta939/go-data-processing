e054bdbbcea1a45d1aad908296cd5ff2feb375c3
odeta939 <odetapenikaite@gmail.com>
2025-08-30 23:48:15 +0200

    add csv parser + add all the checks for report

---
+++ README.md
@@ -0,0 +1,18 @@
+### Kodeoppgave Folio
+
+## Oppgave : 
+Gå gjennom handelsdata fra New Zeland og regne ut følgende: 
+- handels diff mellom import og export
+- mest importerte produktet med verdien av dette
+- mest eksporterte produktet med verdien av dette
+
+- Rapporten skal ha tall fra Norge
+- Rapporten skal ha tall fra 27 EU land hver for seg
+- Rapporten skal ha tall fra EU landene samlet
+
+Krav:
+- Pragrammet skal inneholde kun produkter av type (goods_classification.csv) "goods"
+- Programmet skal inneholde kun produkter som har "NZHSC_Level_2_Code_HS4" (goods_classification.csv) deffinert
+- Programmet skal ignorer linjer med tom "value" kolonnen.
+- Rapporten skal inneholde bare data fra 2024
+
--- main.go
+++ main.go
@@ -1,7 +1,84 @@
-package godataprocessing
+package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	//-------------- DATA FILES -------------//
+	CountryClassification = "/home/odeta/Documents/projects/go-data-processing/data/country_classification.csv"
+	GoodsClassification   = "/home/odeta/Documents/projects/go-data-processing/data/goods_classification.csv"
+	OutputCSVFull         = "/home/odeta/Documents/projects/go-data-processing/data/output_csv_full.csv"
+	Revised               = "/home/odeta/Documents/projects/go-data-processing/data/revised.csv"
+	ServiceClassification = "/home/odeta/Documents/projects/go-data-processing/data/service_classification.csv"
+
+	//--------------OTHER CONSTANTS-----------------//
+	YearToReport = "2024"
+)
+
+const (
+	time_ref = iota
+	account
+	code
+	country_code
+	product_type
+	value
+	status
+)
 
 func main() {
-	fmt.Print("hei")
+	type Record struct {
+		TimeRef     string
+		Account     string
+		Code        string
+		CountryCode string
+		ProductType string
+		Value       float64
+		Status      string
+	}
+	file, err := os.Open(OutputCSVFull)
+	if err != nil {
+		fmt.Println("while opening file ", OutputCSVFull, err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	r, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println("while reading records", err)
+	}
+
+	records := make([]Record, 0)
+	for _, row := range r[1:] {
+
+		isYearValid := row[time_ref][:4] == YearToReport
+		codeValid := len(row[code]) == 4
+		isTypeGoods := row[product_type] == "goods"
+		valueValid := row[value] != ""
+
+		if !isYearValid || !codeValid || !isTypeGoods || !valueValid {
+			continue
+		}
+
+		valString := row[value]
+		val, err := strconv.ParseFloat(valString, 64)
+		if err != nil {
+			fmt.Println("parsing value to float", err)
+		}
+		record := Record{
+			TimeRef:     row[time_ref],
+			Account:     row[account],
+			Code:        row[code],
+			CountryCode: row[country_code],
+			ProductType: row[product_type],
+			Value:       val,
+			Status:      row[status],
+		}
+
+		records = append(records, record)
+	}
 }
